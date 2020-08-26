e4bbdf4eeaf1dd23b16fbe69e8552700387bc5b9
Aileen Rae <aileen@stormideas.com>
2020-08-26 15:21:02 +0100

    Complete QuickStart tutorial setup

--- TestAppConfig/Program.cs
+++ TestAppConfig/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace TestAppConfig
 {
@@ -18,9 +13,12 @@ namespace TestAppConfig
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            .ConfigureWebHostDefaults(webBuilder =>
+            webBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var settings = config.Build();
+                config.AddAzureAppConfiguration(settings["ConnectionStrings:AppConfig"]);
+            })
+            .UseStartup<Startup>());
     }
 }
